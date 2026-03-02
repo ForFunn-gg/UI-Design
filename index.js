9082bec6b3df85f5e361176eff2ab3c49d0e6122
ForFunn-gg <dmaronjake@gmail.com>
2026-03-03 03:16:59 +0800

    day 5 commit

--- index.js
+++ index.js
@@ -13,3 +13,16 @@ document.getElementById('contact-form').addEventListener('submit', function(e){
     Status.className = "success-msg";
     this.reset(); 
 }); 
+
+const btn = document.getElementById('read-more-btn');
+const moreInfo = document.getElementById('more-info');
+
+btn.addEventListener('click', function(){
+    if(moreInfo.classList.contains('hidden')){
+        moreInfo.classList.remove('hidden');
+        btn.textContent = 'Read Less';
+    }else{
+        moreInfo.classList.add('hidden');
+        btn.textContent = 'Read My Story';
+    }
+});
