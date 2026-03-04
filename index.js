c5a582093f9722367ca998193ba7974978ed6c01
ForFunn-gg <dmaronjake@gmail.com>
2026-03-04 22:58:03 +0800

    day 6 commit

--- index.js
+++ index.js
@@ -26,3 +26,43 @@ btn.addEventListener('click', function(){
         btn.textContent = 'Read My Story';
     }
 });
+
+const modal = document.getElementById("project-modal");
+const closeBtn = document.querySelector(".close-btn");
+
+
+closeBtn.addEventListener('click', () => {
+    modal.style.display = "none";
+});
+
+
+window.addEventListener('click', (e) => {
+    if (e.target === modal) {
+        modal.style.display = "none";
+    }
+});
+
+
+document.querySelectorAll('.project-card .btn').forEach(button => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault(); 
+        
+       
+        const card = button.closest('.project-card');
+        const title = card.querySelector('h3').innerText;
+        const desc = card.querySelector('p').innerText;
+        
+      
+        const imgDiv = card.querySelector('.project-img');
+        const bgImg = imgDiv.style.backgroundImage.slice(5, -2); 
+
+       
+        document.getElementById('modal-title').innerText = title;
+        document.getElementById('modal-desc').innerText = desc;
+        document.getElementById('modal-img').src = bgImg;
+
+       
+        modal.style.display = "flex";
+    });
+});
+
